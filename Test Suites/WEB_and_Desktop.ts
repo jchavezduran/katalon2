40563fdbc69ec7031f150e7099d81bc4f159b1d7
jchavezduran <jchavezduran@USAUSJCHAVEZD01.us.deloitte.com>
2023-01-20 23:41:01 -0600

    mixandmatch

---
+++ Test Suites/WEB_and_Desktop.ts
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>WEB_and_Desktop</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations/>
+</TestSuiteCollectionEntity>
