c20788c78d93d15348c2990d3cd93232e1429074
jchavezduran <jchavezduran@USAUSJCHAVEZD01.us.deloitte.com>
2023-01-20 23:06:08 -0600

    WIN

--- Test Suites/DesktopApp_Notepad.ts
+++ Test Suites/DesktopApp_Notepad.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>ceadfdbc-4376-40db-ae28-ac6f7e8745dc</testSuiteGuid>
+   <testCaseLink>
+      <guid>1615e9a9-24e0-4737-87c2-222da6ac39b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Desktop/Desktop_Notepad</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
